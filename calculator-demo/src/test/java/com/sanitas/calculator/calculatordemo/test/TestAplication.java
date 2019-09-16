7dbe20551e5686a4a1246fa0f0e9de5c7f4cc3bf
jarojasa <jarojasa@minsait.com>
2019-09-16 08:28:02 +0200

    Creation of the project structure

---
+++ calculator-demo/src/test/java/com/sanitas/calculator/calculatordemo/test/TestAplication.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.sanitas.calculator.calculatordemo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAplication {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
